c76a45a9d5b97bb6a1594c0b8776ff607c29587a
TJ Balon <balon@tjbalon.io>
2023-02-22 15:07:26 -0500

    CASE Autodocs router + wsgi/reqs
    Signed-off-by: TJ Balon <balon@tjbalon.io>

---
+++ router/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, redirect, abort
+from datetime import datetime
+import string
+import json
+
+app = Flask(__name__)
+
+# load the current version of the mapping cache
+with open('../version_mappings_cache.json', 'r') as fp:
+    mappings = json.load(fp)
+
+@app.route("/")
+def root():
+    '''Routes the root '/' of the host to the index of the docs'''
+
+    # redirect when we find a match based on SSL
+    if request.is_secure:
+        return redirect(f'https://{request.host}/case/documentation/index.html', 301)
+    else:
+        return redirect(f'http://{request.host}/case/documentation/index.html', 301)
+
+@app.route("/<ontology>/<path:target>", methods=['GET'])
+def router(ontology: str, target: str):
+    '''Routes data through the file system to the appropriate documentation'''
+
+    # redirect when we find a match based on SSL
+    if f"{ontology}/{target}" in mappings:
+        location = mappings[f"{ontology}/{target}"]
+        if request.is_secure:
+            return redirect(f'https://{request.host}/{location}', 301)
+        else:
+            return redirect(f'http://{request.host}/{location}', 301)
+    else:
+        abort(401)
