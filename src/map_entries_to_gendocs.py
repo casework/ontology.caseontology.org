97244ade588f9430bc7f86fde237b483fbccd8e2
Alex Nelson <alexander.nelson@nist.gov>
2023-03-16 17:00:12 -0400

    Add NIST license text
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- src/map_entries_to_gendocs.py
+++ src/map_entries_to_gendocs.py
@@ -4,6 +4,21 @@
 # This software was produced for the U.S. Government under contract FA8702-22-C-0001,
 # and is subject to the Rights in Data-General Clause 52.227-14, Alt. IV (DEC 2007)
 # ©2021 The MITRE Corporation. All Rights Reserved.
+#
+#
+# Portions of this file contributed by NIST are governed by the following
+# statement:
+#
+# This software was developed at the National Institute of Standards
+# and Technology by employees of the Federal Government in the course
+# of their official duties. Pursuant to title 17 Section 105 of the
+# United States Code this software is not subject to copyright
+# protection and is in the public domain. NIST assumes no
+# responsibility whatsoever for its use by other parties, and makes
+# no guarantees, expressed or implied, about its quality,
+# reliability, or any other characteristic.
+#
+# We would appreciate acknowledgement if the software is used.
 
 import argparse
 import logging
